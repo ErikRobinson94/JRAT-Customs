8d33b58e304236577736a9b81c554530ec5612fe
ErikRobinson94 <erikdrobins@gmail.com>
2023-07-07 09:33:20 -0700

    added progress bar js

--- script.js
+++ script.js
@@ -25,13 +25,18 @@ e.onmouseout = function() {
  let rightArrow = document.getElementById('rightArrow');
  let rightCounter = document.getElementById('rightCount')
  let leftcount = document.getElementById('leftCount');
+ let myprogress = document.getElementById('progress')
 
  counter = 0
  function slide () {
   if (counter < 10) {
     leftcount.innerHTML = ++counter;
+    let widhthpercent = `${counter*10}%`
+    myprogress.style.width = `${widhthpercent}`;
+
   } else {
     leftcount.innerHTML = 0;
+    myprogress.style.width = "1%";
     counter = 0;
   }
  };
