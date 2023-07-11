67beb28d21636060e6e001900b9c62243fe3a60d
ErikRobinson94 <erikdrobins@gmail.com>
2023-07-11 15:52:43 -0700

    Added: slider functionality, specs show / hide

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
+//contact us button
 let contact = document.getElementById("Contact")
-
-
 function contactChange() {
     contact.classList.add("buttonHover");
 };
@@ -12,48 +11,53 @@ function contactChangeRemove() {
 contact.addEventListener('mouseover', contactChange);
 contact.addEventListener('mouseout', contactChangeRemove);
 
-let e = document.getElementById('information');
-e.onmouseover = function() {
-  document.getElementById('popup').style.display = 'block';
-  document.getElementById('popup').style.color = 'white';
-}
-
-e.onmouseout = function() {
-    document.getElementById('popup').style.display = 'none';
-  }
+//slide bar
 
  let rightArrow = document.getElementById('rightArrow');
- let rightCounter = document.getElementById('rightCount')
- let leftcount = document.getElementById('leftCount');
- let myprogress = document.getElementById('progress')
+ let myprogress = document.getElementById('progress');
+ let count = 0;
 
- counter = 0
- function slide () {
-    leftcount.innerHTML = ++counter;
-    let widhthpercent = `${counter*10}%`
-    myprogress.style.width = `${widhthpercent}`;
-  if (counter == 1) {
-    document.getElementById('biketwo').style.display = "flex";
-    document.getElementById('chris').style.display = "block";
-    document.getElementById('shinyBike').style.display = "none";
-    document.getElementById('bike').style.display = "none";
-    
+ function changeinner () {
+  if (count <10) {
+    count = ++count;
+    document.getElementById("leftCount").innerHTML = `${count}`;
+    let widthPercent = count *10;
+    myprogress.style.width = `${widthPercent}%`
 
-  } else if (counter == 2) {
-    document.getElementById('biketwo').style.display = "none";
-    document.getElementById('chris').style.display = "none";
-    document.getElementById('shinyBike').style.display = "block";
-    document.getElementById('bike').style.display = "flex";
-    
-
-  }
-  
-  
-  else {
-    leftcount.innerHTML = 0;
+  } else {
+    count = 0;
+    document.getElementById("leftCount").innerHTML = `0`;
     myprogress.style.width = "1%";
-    counter = 0;
+
   }
  };
 
-rightArrow.addEventListener('click', slide);
+ rightArrow.addEventListener("click", changeinner)
+
+ // add specs
+ let specs = document.getElementById("specifications");
+ let gal = document.getElementById("gallery");
+ let specDetail = document.getElementById("specDetail");
+ let siz = document.getElementById("sizzle");
+ 
+
+
+ function showSpecs () {
+  siz.style.display="none";
+  specDetail.style.display="block";
+  specs.style.backgroundColor = 'red';
+
+
+ };
+
+ function ag() {
+  specs.style.backgroundColor = 'black';
+  siz.style.display="block";
+  specDetail.style.display="none";
+
+ };
+
+
+ specs.addEventListener("mouseover", showSpecs);
+ specs.addEventListener('mouseout', ag);
+ 
