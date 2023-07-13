986c93249e82920a0e675280f3d67bdbfa95df5b
ErikRobinson94 <erikdrobins@gmail.com>
2023-07-13 11:22:43 -0700

    attempt

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // mobile issue
 const appHeight = () => {
   const doc = document.documentElement
-  doc.style.setProperty(‘ — app-height’, `${window.innerHeight}px`)
- }
- window.addEventListener(‘resize’, appHeight)
- appHeight()
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight()
 
 
 //contact us button
